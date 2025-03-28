cb3e011e795eaf1ee0c2929cdea91faa335aeaf4
sharoncr98 <chrysler2102@gmail.com>
2025-03-27 23:15:49 -0400

    Fixed the URL request

--- js/json.js
+++ js/json.js
@@ -6,7 +6,7 @@ const section = document.querySelector('section');
 async function populate(){
     // Introducing JavaScript Object Notation (JSON): https://json.org/
     // STEP 4: Store the URL of a JSON file in a variable */
-    const requestURL = 'https://raw.githubusercontent.com/sharoncr98/COMP1073-01-Lab-4/refs/heads/main/js/i-scream.json?token=GHSAT0AAAAAAC4636AXA5BWWYT5ZCJ2UHN2Z7GBXDA';
+    const requestURL = 'https://raw.githubusercontent.com/sharoncr98/COMP1073-01-Lab-4/refs/heads/main/js/i-scream.json';
     // STEP 5: Use the new URL to create a new request object
     const request = new Request(requestURL);
     // STEP 6: Make a network request with the fetch() function, which returns a Response object
@@ -55,9 +55,9 @@ function showTopFlavors(jsonObj) {
 
         // STEP 10f: Set the textContent property for each of the above elements (except the UL), based on the JSON content
         h2.textContent = flavorName;
-        image.setAttribute('src','https://raw.githubusercontent.com/sharoncr98/COMP1073-01-Lab-4/refs/heads/main/images/' + topFlavors[i].image)
+        image.setAttribute('src','https://raw.githubusercontent.com/shiv-coder/COMP1073W/main/lesson-10/lesson-10/images/' + topFlavors[i].image)
 
-        // STEP 10g: Build a loop for the ingredients array in the JSON 
+        // STEP 10g: Build a loop for the ingredients array in the JSON
         let ingredients = topFlavors[i]["ingredients"];
         for(let j=0;j<ingredients.length;j++){
             // add the ingredient to the UL
